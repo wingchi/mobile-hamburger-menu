068eec8cbc4bceddd0daffe9af8524dbd8b6dc42
Stephen Wingchi Wong <stephwwong@gmail.com>
2016-11-21 10:51:37 -0500

    Toggle nav classes based on useAgent

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,31 @@
 // Javascript to toggle hamburger menu goes here
+function detectmob() {
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+if(detectmob()) {
+  var navs = document.querySelectorAll(".nav-section");
+  var navItems = document.querySelectorAll(".nav-item");
+  for(let nav of navs) {
+    nav.classList.add("nav-section-mob");
+    nav.classList.remove("nav-section");
+  }
+  for(let navItem of navItems) {
+    navItem.classList.add("nav-item-mob");
+    navItem.classList.remove("nav-item");
+  }
+} else {
+  alert("desktop");
+}
